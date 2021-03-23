eac589adfc2f1f873edc81775b04ce4eeabfeca3
PrajeetGuha <prajeetguha@gmail.com>
2021-03-23 11:59:38 +0530

    sign_in and login updates

--- dependencies/utility.py
+++ dependencies/utility.py
@@ -6,3 +6,19 @@ def random_captcha():
     s = 8
     sentence = ''.join(random.choices(string.ascii_uppercase + string.digits, k = s))
     return sentence
+
+def registration_status_code(var_dict):
+    
+    #status_dict = {0:'Wrong Captcha', 1:'Username Invalid', 2:'Username Previously Registered', 3:'Password should be atleast 8 character long', 4:'Registration Done Successfully'}
+    
+    #return the status_code only
+    
+    pass
+
+def login_status_code(var_dict):
+    
+    #status_dict = {0:'Wrong Captcha', 1:'Invalid Credentials'}
+    
+    #return the status_code only
+    
+    pass
